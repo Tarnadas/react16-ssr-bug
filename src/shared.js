0a3628ac000a5def851dce18913a3ef8d5fb7db4
Mario <m_reder10@cs.uni-kl.de>
2017-09-05 10:06:52 +0200

    List Component bug

--- src/shared.js
+++ src/shared.js
@@ -7,22 +7,59 @@ export default function renderer (isServer, render) {
 class AppView extends React.Component {
   render () {
     const isServer = this.props.isServer
-    console.log(isServer)
-    const styles = {
-      server: {
-        backgroundColor: 'red'
-      },
-      client: {
-        backgroundColor: 'green'
-      }
-    }
     return (
       <div>
         {
-          isServer ?
-            <div style={styles.server}>isServer</div> :
-            <div style={styles.client}>isClient</div>
+          isServer && <ServerComponent />
         }
+        <ListComponent />
+      </div>
+    )
+  }
+}
+
+class ServerComponent extends React.Component {
+  render () {
+    const styles = {
+      server: {
+        backgroundColor: 'red'
+      }
+    }
+    return <div style={styles.server} />
+  }
+}
+
+class ListComponent extends React.Component {
+  constructor (props) {
+    super(props)
+    this.getList = this.getList.bind(this)
+  }
+  getList () {
+    const styles = {
+      el: {
+        width: '200px',
+        height: '200px',
+        margin: '20px',
+        backgroundColor: 'blue'
+      }
+    }
+    return Array.from((function * () {
+      for (let i = 0; i < 10; i++) {
+        yield <div style={styles.el} key={i} />
+      }
+    })())
+  }
+  render () {
+    const styles = {
+      client: {
+        backgroundColor: 'green',
+        display: 'flex',
+        flexWrap: 'wrap'
+      }
+    }
+    return (
+      <div style={styles.client}>
+        { this.getList() }
       </div>
     )
   }
